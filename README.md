6511f291a065ca50da61a0688de018a87e1ed5ac
i5heu <i5heu@i5h.eu>
2017-05-01 08:57:39 +0200

    Rename files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # wiki_cannon
+this is a simple to install and run private Wiki!
---
+++ wc-newentry.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func NewentryHandler(w http.ResponseWriter, r *http.Request) {
+
+	t := "login: false"
+
+	if checkLogin(r) == true {
+		//if true == true {
+		t = "login: true"
+
+		newTitle := r.FormValue("Title")
+		newNamepace := r.FormValue("Namespace")
+		newText := r.FormValue("Text")
+		db.Exec("INSERT INTO article(title,namespace,text) VALUES(?,?,?)", ReplaceSpecialChars(newTitle), ReplaceSpecialChars(newNamepace), newText)
+
+		checkErr(err)
+
+		http.ServeFile(w, r, "newentry.html")
+
+	}
+	fmt.Fprintf(w, `You have to login to do this! -> %s`, t)
+
+}
